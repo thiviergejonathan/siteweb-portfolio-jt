fac0b90a45624b978653dec3a5219d724a8de92a
Jonathan Thivierge <thiviergejonathan@gmail.com>
2023-02-06 17:29:32 -0500

    testes ficher TS

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-// test
+var testButton = document.getElementById('testButton');
+var testDiv = document.getElementById('testDiv');
+if (testButton) {
+    testButton.onclick = function () { return alert('oops!'); };
+}
--- js/scripts.ts
+++ js/scripts.ts
@@ -1,1 +1,6 @@
-// test
+const testButton = document.getElementById('testButton');
+const testDiv = document.getElementById('testDiv');
+
+if (testButton) {
+    testButton.onclick = () => alert('oops!');
+}
